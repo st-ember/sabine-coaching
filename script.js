0685e97d9c8379e7d8440fbe36dc5db73f75b82e
st-ember <86178430+st-ember@users.noreply.github.com>
2023-02-06 14:03:18 +0800

    added new files

---
+++ script.js
@@ -0,0 +1,6 @@
+$('ul.nav').find('a').click(function(){
+    var $href = $(this).attr('href');
+    var $anchor = $('#'+$href).offset();
+    window.scrollTo($anchor.left,$anchor.top);
+    return false;
+});
